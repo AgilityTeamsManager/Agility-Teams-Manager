b78241702537c0fd96fe9d4e78aa1a8507788893
Virinas-code <alexcode228@gmail.com>
2022-01-26 14:41:25 +0100

    Login: Login page, will be refactorized

--- app.py
+++ app.py
@@ -5,19 +5,29 @@ PROGESCO Teams.
 
 Classements par équipe pour PROGESCO.
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/login")
 def index():
     """
     Main app page.
 
     Home screen and app page.
     """
-    return render_template("index.html")
+    return render_template("login.html")
+
+
+@app.route("/manifest.json")
+def manifest():
+    """
+    Manifest file.
+
+    Redirects to static path.
+    """
+    return redirect("/static/manifest.json", 301)
 
 
 if __name__ == "__main__":
