0ada50dd031f8c7543406c36d0b6a3d7d4c184d4
Virinas-code <alexcode228@gmail.com>
2022-01-27 19:09:37 +0100

    Login: Base of signup. Nothing works yet.

--- app.py
+++ app.py
@@ -7,8 +7,12 @@ Classements par équipe pour PROGESCO.
 """
 import _csv
 import csv
+import email.mime.multipart
+import email.mime.text
 import hashlib
 import os
+import smtplib
+import uuid
 
 from flask import Flask, render_template, redirect, request, session
 
@@ -18,6 +22,11 @@ with open("data/users.csv") as f:
     passwords_file: _csv.reader = csv.reader(f)
     for row in passwords_file:
         users[row[0]] = row[1]
+
+smtp_server: smtplib.SMTP_SSL = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+smtp_server.login("progesco.teams@gmail.com", os.environ["SECRET_GMAIL"])
+
+signups: dict[str, dict[str, str]] = {}
 
 app.secret_key = os.environ["SECRET_KEY"]
 
@@ -40,6 +49,35 @@ def index():
         else:
             return render_template("login.html", error="Utilisateur inconnu")
     return render_template("login.html")
+
+
+@app.route("/account/signup/<uuid:id_confirm>")
+def signup_confirm(id_confirm):
+    """
+    Confirm signup.
+    """
+    
+
+
+@app.route("/account/signup", methods=["POST"])
+def signup():
+    """
+    Signup.
+
+    Send an email.
+    """
+    random_uuid: str = uuid.uuid4().hex
+    signups[random_uuid] = {"user": request.form["user"], "password": hashlib.sha256(request.form["password"].encode()).hexdigest()}
+    mail = email.mime.multipart.MIMEMultipart("alternative")
+    mail["Subject"] = "PROGESCO Teams - Confirmer l'inscription"
+    mail["From"] = "progesco.teams@gmail.com"
+    mail["To"] = request.form["user"]
+    confirm_url: str = request.url_root + "account/signup/" + random_uuid
+    mail.attach(email.mime.text.MIMEText(f"Suivez ce lien pour confirmer votre inscription : {confirm_url}", "plain"))
+    with open("signup_mail.html") as file:
+        mail.attach(email.mime.text.MIMEText(file.read(), "html"))
+    smtp_server.sendmail("progesco.teams@gmail.com", request.form["user"], mail.as_string())
+    return render_template("signup.html")
 
 
 @app.route("/manifest.json")
