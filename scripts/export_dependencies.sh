49116ad075206856e188e169720435a9f373d366
Virinas-code <alexcode228@gmail.com>
2022-07-08 01:42:05 +0200

    Dev requirements

--- scripts/export_dependencies.sh
+++ scripts/export_dependencies.sh
@@ -2,3 +2,4 @@
 #TODO: GitHub Action
 poetry lock --no-update -q --no-ansi -n
 poetry export -f requirements.txt -o requirements.txt --without-hashes -q --no-ansi -n
+poetry export -f requirements.txt --dev -o requirements-dev.txt --without-hashes -q --no-ansi -n
